ae35003f486147ae7aa906c2b54d6f62b099e1a2
KingOfGitH <2181820@mail.dhu.edu.cn>
2019-03-23 12:21:53 +0800

    注册和登陆界面
    1.0

---
+++ webtest/LoginUI/LoginUI.js
@@ -0,0 +1,14 @@
+function showUserName() {
+    var info=document.getElementById("123");
+    info.innerHTML="用户名是由字母数字下划线组成";
+
+}
+function register() {
+    window.location.href='http://www.baidu.com';
+}
+function formReset()
+{
+    document.getElementById("userName").reset();
+    document.getElementById("password").reset();
+    document.getElementById("password").reset();
+}
---
+++ webtest/test.js
@@ -0,0 +1,3 @@
+var str = new String("Hello world");
+
+alert(str.fontcolor( "red" ));
