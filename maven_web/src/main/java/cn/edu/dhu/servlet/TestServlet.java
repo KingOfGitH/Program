311977fd710b043127ba366799a4f671350f8a56
KingOfGitH <2181820@mail.dhu.edu.cn>
2019-04-09 22:53:52 +0800

    maventest

---
+++ maven_web/src/main/java/cn/edu/dhu/servlet/TestServlet.java
@@ -0,0 +1,17 @@
+package cn.edu.dhu.servlet;
+
+import java.io.IOException;
+
+/**
+ * @author zhbr
+ * @date 2019/4/9 17:09
+ */
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher("/index.jsp").forward(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request,response);
+    }
+}
